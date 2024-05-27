24795f3b503a71126ffe745129f33bcda2a5e80c
VeiaG <romapalamar.veiag@gmail.com>
2024-05-27 18:57:59 +0300

    responsive & hover effect

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ avatars.forEach((avatar) => {
   shadow.classList.add('avatar__shadow');
   if(avatar.parentElement) avatar.parentElement.appendChild(shadow);
 });
+
+// const menuButton = document.querySelector<HTMLDivElement>('.header__menu-btn');
+// const navigation =  document.querySelector<HTMLDivElement>('.header__nav');
+// menuButton?.addEventListener('click', () => {
+//   navigation?.classList.toggle('opened');
+//   menuButton?.classList.toggle('opened');
+// });
